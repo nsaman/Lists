481a04b9f0e5a8061e36960c784587f333cdbc7c
Nick Carroll-Anderson <emailofnick@gmail.com>
2020-10-04 01:09:41 -0500

    Removing spring because it ain't working out

--- src/main/java/com/lists/handler/DynamoThingStreamHandler.java
+++ src/main/java/com/lists/handler/DynamoThingStreamHandler.java
@@ -1,42 +1,53 @@
 package com.lists.handler;
 
+import com.amazonaws.auth.AWS4Signer;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.lists.config.AWSRequestSigningApacheInterceptor;
 import com.lists.model.Thing;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequestInterceptor;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.*;
+import java.util.Map;
 
 /**
  * Handler for requests to Lambda function.
  */
 @Slf4j
 public class DynamoThingStreamHandler implements RequestHandler<DynamodbEvent, Void> {
+    private static final String ELASTIC_SEARCH_NAME = "es";
 
     private static final String INSERT = "INSERT";
     private static final String MODIFY = "MODIFY";
     private static final String REMOVE = "REMOVE";
 
-    @Autowired
     private RestHighLevelClient restHighLevelClient;
     private Gson gson;
 
     public DynamoThingStreamHandler() {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext("com.lists");
-        ctx.getAutowireCapableBeanFactory().autowireBean(this);
+        String elasticEndpoint = System.getenv("elasticEndpoint");
+        String region = System.getenv("AWS_REGION");
+        AWS4Signer signer = new AWS4Signer();
+        signer.setServiceName(ELASTIC_SEARCH_NAME);
+        signer.setRegionName(region);
+        HttpRequestInterceptor interceptor = new AWSRequestSigningApacheInterceptor(ELASTIC_SEARCH_NAME, signer, new DefaultAWSCredentialsProviderChain());
+        restHighLevelClient = new RestHighLevelClient(RestClient.builder(HttpHost.create(elasticEndpoint)).setHttpClientConfigCallback(hacb -> hacb.addInterceptorLast(interceptor)));
         gson = new Gson();
     }
 
