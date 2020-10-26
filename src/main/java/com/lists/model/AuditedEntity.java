39bae82ec7ed371f06dc24eecfaadf41eb3c983d
Nick Carroll-Anderson <emailofnick@gmail.com>
2020-10-25 19:59:43 -0500

    Trying a fix for parents attributes not being saved

--- src/main/java/com/lists/model/AuditedEntity.java
+++ src/main/java/com/lists/model/AuditedEntity.java
@@ -1,5 +1,6 @@
 package com.lists.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperFieldModel;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTyped;
@@ -9,6 +10,7 @@ import lombok.Data;
  * Created by nick on 10/9/2018.
  */
 @Data
+@DynamoDBDocument
 public abstract class AuditedEntity {
     @DynamoDBTyped(DynamoDBMapperFieldModel.DynamoDBAttributeType.S)
     private String createUserID;
